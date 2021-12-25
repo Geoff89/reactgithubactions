4cfa8fd84182689c80eca4d1910eaabc0c308c2a
Geoffrey <saginigeoffrey@gmail.com>
2021-12-25 13:02:05 +0300

    router changes in react

--- src/App.js
+++ src/App.js
@@ -1,33 +1,20 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-
-          <li>
-            <Link to="/dashboard">Dashboard</Link>
-          </li>
-        </ul>
-        <hr />
-
-        <Switch>
-          <Route exact path="/">
-            {" "}
-            <Homepage />
-          </Route>
-          <Route path="/dashboard">
-            {" "}
-            <Dashboard />
-          </Route>
-        </Switch>
-      </div>
+      <Switch>
+        <Route exact path="/">
+          {" "}
+          <Homepage />
+        </Route>
+        <Route path="/dashboard">
+          {" "}
+          <Dashboard />
+        </Route>
+      </Switch>
     </Router>
   );
 }
